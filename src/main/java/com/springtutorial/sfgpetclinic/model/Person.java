506bf87ba792cae68bfd40a41cedc3af6928d2f7
shyam.k <shyamk.iiit@gmail.com>
2018-10-25 17:41:34 +0530

    addin base entity. closes #10

--- src/main/java/com/springtutorial/sfgpetclinic/model/Person.java
+++ src/main/java/com/springtutorial/sfgpetclinic/model/Person.java
@@ -1,6 +1,7 @@
 package com.springtutorial.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
+
 
     private String firstName;
     private String lastName;
--- src/main/java/com/springtutorial/sfgpetclinic/model/PetType.java
+++ src/main/java/com/springtutorial/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.springtutorial.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     String name;
 
