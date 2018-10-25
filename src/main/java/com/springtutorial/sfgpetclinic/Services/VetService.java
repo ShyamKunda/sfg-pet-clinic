98bbfd0e44f801c1959e85c079c3ddadcaff895d
shyam.k <shyamk.iiit@gmail.com>
2018-10-25 17:28:46 +0530

    Implement Intefaces for services. Closes #5.

---
+++ src/main/java/com/springtutorial/sfgpetclinic/Services/VetService.java
@@ -0,0 +1,14 @@
+package com.springtutorial.sfgpetclinic.Services;
+
+import com.springtutorial.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
